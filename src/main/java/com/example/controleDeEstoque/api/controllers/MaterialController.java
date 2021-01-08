07e37ef05b486884af0e3e1c6085858987cbfc6a
AndressaMesquita <rib.mesq.Andressa72@gmail.com>
2021-01-08 11:24:40 -0300

    Adição de método get para listar todos os materiais

--- src/main/java/com/example/controleDeEstoque/api/controllers/MaterialController.java
+++ src/main/java/com/example/controleDeEstoque/api/controllers/MaterialController.java
@@ -1,9 +1,12 @@
 package com.example.controleDeEstoque.api.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.example.controleDeEstoque.api.dtos.requests.MaterialDTORequest;
 import com.example.controleDeEstoque.api.dtos.responses.MaterialDTOResponse;
 import com.example.controleDeEstoque.domain.models.Material;
+import com.example.controleDeEstoque.domain.repositories.MaterialRepository;
 import com.example.controleDeEstoque.domain.services.MaterialService;
 
 @RestController
@@ -21,6 +25,9 @@ public class MaterialController {
 
 	@Autowired
 	MaterialService materialService;
+
+	@Autowired
+	MaterialRepository materialRepository;
 
 	@PostMapping(path = "/materiais")
 	public ResponseEntity<MaterialDTOResponse> cadastrar(@Valid @RequestBody MaterialDTORequest materialDTORequest,
@@ -32,8 +39,18 @@ public class MaterialController {
 		MaterialDTOResponse materialDTOResponse = new MaterialDTOResponse(materialSalvo);
 
 		return ResponseEntity
-				.created(uriComponentsBuilder.path("/materiais/{id}").buildAndExpand(material.getId()).toUri())
+				.created(uriComponentsBuilder.path("/materiais/{id}").buildAndExpand(materialSalvo.getId()).toUri())
 				.body(materialDTOResponse);
 	}
+
+	@GetMapping(path = "/materiais/lista")
+	public List<Material> listarMateriais() {
+		return materialRepository.findAll();
+	}
+
+//	@GetMapping(path = "/materiais/{id}")
+//	public Optional<Material> buscarPorId(Long id) {
+//		return materialRepository.findById(id);
+//	}
 
 }
