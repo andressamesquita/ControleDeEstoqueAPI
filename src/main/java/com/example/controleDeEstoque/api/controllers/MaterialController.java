e53bd3511c5bdd47a3d6bd251bcbd04e82bff014
AndressaMesquita <rib.mesq.Andressa72@gmail.com>
2021-01-08 22:22:17 -0300

    Correção na busca de materiais por id

--- src/main/java/com/example/controleDeEstoque/api/controllers/MaterialController.java
+++ src/main/java/com/example/controleDeEstoque/api/controllers/MaterialController.java
@@ -1,12 +1,14 @@
 package com.example.controleDeEstoque.api.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,9 +50,9 @@ public class MaterialController {
 		return materialRepository.findAll();
 	}
 
-//	@GetMapping(path = "/materiais/{id}")
-//	public Optional<Material> buscarPorId(Long id) {
-//		return materialRepository.findById(id);
-//	}
+	@GetMapping(path = "/materiais/id/{id}")
+	public Optional<Material> buscarPorId(@PathVariable Long id) {
+		return materialRepository.findById(id);
+	}
 
 }
